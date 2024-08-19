532ab49f57b91790faedb150394e8c8a49e9e79e
sol-un <solovyev833@gmail.com>
2024-08-19 23:07:35 +0300

    test_each_model_has_its_own_attributes passed

--- ruby-basics-ru/metaprogramming/lib/model.rb
+++ ruby-basics-ru/metaprogramming/lib/model.rb
@@ -2,28 +2,39 @@
 
 # BEGIN
 module Model
+  def coerce(value, type)
+    case type
+    when :string
+      value.to_s
+    when :integer
+      value.to_i
+    when :boolean
+      !!value
+    when :datetime
+      DateTime.parse(value)
+    else
+      value
+    end
+  end
+
   def self.included(base)
     base.extend(ClassMethods)
   end
 
   module ClassMethods
     def attribute(name, options = {})
+      define_method 'initialize' do |attributes = {}|
+        attributes.each do |key, value|
+          instance_variable_set "@#{key}", value
+        end
+      end
+
       define_method name do
         instance_variable_get "@#{name}"
       end
 
       define_method "#{name}=" do |value|
-        coerced_value = case options[:type]
-                        when :string
-                          value.to_s
-                        when :integer
-                          value.to_i
-                        when :boolean
-                          !!value
-                        when :datetime
-                          DateTime.parse(value)
-                        end
-        instance_variable_set "@#{name}", coerced_value
+        instance_variable_set "@#{name}", coerce(value, options[:type])
       end
     end
   end
